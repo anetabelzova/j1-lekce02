5ab78f6b7824528695df4637983a5904bc5d6a4f
anetabelzova <anetabelzova@gmail.com>
2023-02-14 20:24:18 +0100

    Cvíčení 1 - 1 domeček a čtverec

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -2,44 +2,52 @@ package cz.czechitas.java.lekce02;
 
 import cz.czechitas.java.lekce02.engine.Turtle;
 
+import java.util.Random;
+
 public class HlavniProgram {
     private Turtle zofka;
     public void start() {
         zofka = new Turtle();
 
-        //TODO tady bude kód
 
-        /*
-        for (int i = 0; i < 4; i++) {
+
+
+
+        for (int i = 0; i < 5; i++) {
+
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
+        zofka.turnLeft(60);
+        zofka.move(100);
+        zofka.turnRight(120);
+        zofka.move(100);
+
+        zofka.penUp();
+        zofka.turnLeft(60);
+        zofka.move(100);
+        zofka.penDown();
+
+
+        var random = new Random();
+        var nakresliCtverec=random.nextInt(4)+1;
+        while (nakresliCtverec!=4){
 
             zofka.move(100);
             zofka.turnRight(90);
 
+
+
+
         }
 
-         */
-/*
-zofka= new Turtle();
-zofka.move(100);
-zofka.turnRight(90);
-zofka.move(100);
-zofka.penUp();
-zofka.turnRight(90);
-zofka.move(100);
-zofka.turnRight(90);
-zofka.penDown();
-zofka.move(100);
 
- */
 
-        nakresliCtverec();
 
-        zofka.penUp();
-        zofka.turnLeft(90);
-        zofka.move(200);
-        zofka.turnRight(90);
 
-        nakresliCtverec();
+
+
+
 
 
 
