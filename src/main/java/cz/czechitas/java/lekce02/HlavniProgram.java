06f0d0d2afa9bf7249049afa3e186c84806d1bd2
anetabelzova <anetabelzova@gmail.com>
2023-02-14 19:45:04 +0100

    Commitnutí společné lekce

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -8,6 +8,41 @@ public class HlavniProgram {
         zofka = new Turtle();
 
         //TODO tady bude kód
+
+        /*
+        for (int i = 0; i < 4; i++) {
+
+            zofka.move(100);
+            zofka.turnRight(90);
+
+        }
+
+         */
+/*
+zofka= new Turtle();
+zofka.move(100);
+zofka.turnRight(90);
+zofka.move(100);
+zofka.penUp();
+zofka.turnRight(90);
+zofka.move(100);
+zofka.turnRight(90);
+zofka.penDown();
+zofka.move(100);
+
+ */
+
+        nakresliCtverec();
+
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(200);
+        zofka.turnRight(90);
+
+        nakresliCtverec();
+
+
+
     }
 
     public static void main(String[] args) {
